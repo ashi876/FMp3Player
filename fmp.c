2270128ca62f16befc120cd40fa0950b91dfd8da
ashi876 <fyq876@163.com>
2017-10-12 09:16:49 +0800

    20171012
    新的功能
    命令[fmp p number]函数实现.从指定歌曲到末尾歌曲

--- fmp.c
+++ fmp.c
@@ -46,7 +46,8 @@ int WINAPI WinMain( HINSTANCE hInstance,
 	int fmp_stop();
 	int listfmp();
 	int fmp_singlerepeat();
-	int fmp_singleplay();	
+	int fmp_singleplay();
+	int fmp_point();	
 
 	//命令[fmp]帮助窗口
 	if(__argc<2) {
@@ -64,18 +65,23 @@ int WINAPI WinMain( HINSTANCE hInstance,
 			listfmp();
 			return 0;
 		}
-		
 		//命令[fmp xxx.mp3]单次播放 
 		fmp_singleplay();
-		return 0;	
+		return 0;
 	}
 
 	//命令[fmp r]单曲循环
-	else if(__argc==3&&(strcmp(__argv[1],"r")==0)) 
-	{  
+	else if(__argc==3&&(strcmp(__argv[1],"r")==0)) {  
 		fmp_singlerepeat();
 		return 0;
 	}
+	
+	//命令[fmp p number mp3]单曲循环
+	else if(__argc==3&&(strcmp(__argv[1],"p")==0)) {  
+		fmp_point();
+		return 0;
+	}
+	
 	return 0;
 }
 
@@ -92,11 +98,55 @@ void sendCommand(char *s)
 	}
 }
 
+
+/************************************************************************/
+//命令[fmp]帮助窗口函数实现
+int fmp_help()
+{
+	//printf("帮助窗口%s %s %s %d ",__argv[0],__argv[1],__argv[2],__argc);		
+	sprintf(msg,"Fmediaplay-%d位版\n命令格式:\n\t[fmp] [参数] [\"路径\\文件名.mp3\"]\n", sizeof(int*)*8);
+	MessageBox(NULL,msg,"FMediaPlay 千城真人",MB_OK);
+	//getch();
+	return 1;
+}
+
+/************************************************************************/
+//命令[fmp s]函数实现
+int fmp_stop()
+{
+	printf("执行关闭命令：%s %s\n",__argv[0],__argv[1]);	
+	char cmd[100];//容纳cmd的字符串变量。
+	sendCommand("Close All");
+	sleep(1);
+	sprintf(cmd, "taskkill /f /t /im %s.exe",__argv[0]);//写入cmd命令
+	//printf("%s",cmd);
+	system(cmd);
+	return 0;
+}
+
+/************************************************************************/
+//命令[fmp xxx.mp3]函数实现
+int fmp_singleplay()
+{  
+	GetShortPathName(__argv[1],shortBuffer,sizeof(shortBuffer));
+	if(!*shortBuffer) {
+		sprintf(msg,"Cannot shorten filename \"%s\"\n",__argv[1]);
+		MessageBox(NULL,msg,"Fmediaplay",MB_OK);
+		return 1;
+	}  
+	sendCommand("Close All");
+	sprintf(cmdBuff,"Open %s Type MPEGVideo Alias theMP3",shortBuffer);
+	sendCommand(cmdBuff);
+	sendCommand("Play theMP3 Wait");
+	//printf("单次\n %s %s %s %d",__argv[0],__argv[1],__argv[2],__argc);
+	return 0;
+}
+
 /************************************************************************/
 //命令[fmp l]函数实现		
 int listfmp()
 {	
-	char cmdBuff[MAX_PATH + 64];
+
 	int i,j;
 	char listname[31][100], *ext[1];//定义显示例表的数组长度,和一个存放后缀名的指针数组
 	ext[1]="*.mp3";//可以用下一句指定后缀名
@@ -133,31 +183,6 @@ int listfmp()
 }
 
 /************************************************************************/
-//命令[fmp]帮助窗口函数实现
-int fmp_help()
-{
-	//printf("帮助窗口%s %s %s %d ",__argv[0],__argv[1],__argv[2],__argc);		
-	sprintf(msg,"Fmediaplay-%d位版\n命令格式:\n\t[fmp] [参数] [\"路径\\文件名.mp3\"]\n", sizeof(int*)*8);
-	MessageBox(NULL,msg,"FMediaPlay 千城真人",MB_OK);
-	//getch();
-	return 1;
-}
-
-/************************************************************************/
-//命令[fmp s]函数实现
-int fmp_stop()
-{
-	printf("执行关闭命令：%s %s\n",__argv[0],__argv[1]);	
-	char cmd[100];//容纳cmd的字符串变量。
-	sendCommand("Close All");
-	sleep(1);
-	sprintf(cmd, "taskkill /f /t /im %s.exe",__argv[0]);//写入cmd命令
-	//printf("%s",cmd);
-	system(cmd);
-	return 0;
-}
-
-/************************************************************************/
 //命令[fmp r xxx.mp3]函数实现
 int fmp_singlerepeat()
 {  
@@ -180,19 +205,50 @@ int fmp_singlerepeat()
 }
 
 /************************************************************************/
-//命令[fmp xxx.mp3]函数实现
-int fmp_singleplay()
-{  
-	GetShortPathName(__argv[1],shortBuffer,sizeof(shortBuffer));
-	if(!*shortBuffer) {
-		sprintf(msg,"Cannot shorten filename \"%s\"\n",__argv[1]);
-		MessageBox(NULL,msg,"Fmediaplay",MB_OK);
-		return 1;
-	}  
-	sendCommand("Close All");
-	sprintf(cmdBuff,"Open %s Type MPEGVideo Alias theMP3",shortBuffer);
-	sendCommand(cmdBuff);
-	sendCommand("Play theMP3 Wait");
-	//printf("单次\n %s %s %s %d",__argv[0],__argv[1],__argv[2],__argc);
-	return 0;
+//命令[fmp p number]函数实现.从指定歌曲到末尾歌曲		
+int fmp_point()
+{	
+
+	int i,j;
+	char listname[31][100], *ext[1];//定义显示例表的数组长度,和一个存放后缀名的指针数组
+	ext[1]="*.mp3";//可以用下一句指定后缀名
+	//scanf("%s",&*ext[1]);
+	
+    WIN32_FIND_DATA p;
+    HANDLE h=FindFirstFile(ext[1],&p);//见文尾注释,本例中在当前目录搜索mp3文件
+    //puts(p.cFileName);//去掉注释，帮你理解此时输出了什么
+	strcpy(listname[1],p.cFileName);//第一次写入数组
+	printf("%d %s\n",1,listname[1]);
+	
+	i=2;//listname[1]赋值过了，listname[0]就让它空着吧
+	while(FindNextFile(h,&p)){
+		//puts(p.cFileName);//去掉注释，帮你理解此时输出了什么
+		strcpy(listname[i],p.cFileName);
+		printf("%d %s\n",i,listname[i]);
+		i++;
+		if(i>30){
+			printf("warning! show 30 songs best!");
+			break;
+		}// i>30为显示和写入listname[]的mp3歌曲数量
+	}
+	
+	
+	j=i;
+	i=atoi(__argv[2]);
+	sprintf(msg,"点击确定将从当前歌曲播放到最后一首:\n\n当前歌曲%d %s\n...\n最后一首%d %s",i,listname[i],j-1,listname[j-1]);
+	//MessageBox(NULL,msg,"fmp",MB_OKCANCEL);
+	if(MessageBox(NULL,msg,"fmp",MB_OKCANCEL)==IDCANCEL)  
+	return 0; 
+	printf("播放的歌曲序号: %d\n", i);
+
+	for(;i<j;i++){
+		sendCommand("Close All");
+		sprintf(cmdBuff,"Open \"%s\" Type MPEGVideo Alias theMP3",listname[i]);
+		sendCommand(cmdBuff);
+		sendCommand("Play theMP3 Wait");
+		//mciSendString("play theMP3 Wait",NULL,0,0);//等同于上条命令
+	}
+	//printf("记录一%s 记录二%s\n",listname[1],listname[3]);//验证数组内容用的
+	//system("pause");
+    return 0;
 }
