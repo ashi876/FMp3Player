ab8642071d3e4e991b749f0ab46745be73991afd
ashi876 <fyq876@163.com>
2017-10-30 09:09:27 +0800

    20171030001
    精简了代码分离播放出函数fmp_play．为2.0版调用开源mpg123库做准备

--- fmp1.1/fmp.c
+++ fmp1.1/fmp.c
@@ -3,7 +3,7 @@
 	> 作者：千城真人
 	> 简介: win下的调用mci函数的mp3播放工具．可以在命令行下执行.
 	> 功能：可以单次和循环播放mp3
-	> 时间：20171012
+	> 时间：20171030
 	
 	> 使用方法：
 		1.将快捷方式放在右键<发送到>菜单，直接把mp3发送到本工具上
@@ -36,7 +36,9 @@ void sendCommand(char *);
 
 char shortBuffer[MAX_PATH];
 char cmdBuff[MAX_PATH + 64];
+char listname[31][100];
 
+int fmp_play();
 /************************************************************************/
 int WINAPI WinMain( HINSTANCE hInstance, 
 					HINSTANCE hPrevInstance, 
@@ -49,7 +51,7 @@ int WINAPI WinMain( HINSTANCE hInstance,
 	int fmp_singlerepeat();
 	int fmp_singleplay();
 	int fmp_point();
-	int fmp_pointrepeat();	
+	int fmp_pointrepeat();
 
 	//命令[fmp]帮助窗口
 	if(__argc<2) {
@@ -111,11 +113,9 @@ void sendCommand(char *s)
 /************************************************************************/
 //命令[fmp]帮助窗口函数实现
 int fmp_help()
-{
-	//printf("帮助窗口%s %s %s %d ",__argv[0],__argv[1],__argv[2],__argc);		
+{	
 	sprintf(msg,"Fmediaplay-%d位版\n命令格式:\n单曲：fmp xxx.mp3\n单曲循环：fmp r xxx.mp3\n指定序号到最后一首:fmp p number\n指定序号循环播放:fmp pr number\n播放全曲：fmp l\n关闭命令：fmp s", sizeof(int*)*8);
 	MessageBox(NULL,msg,"FMediaPlay 千城真人",MB_OK);
-	//getch();
 	return 1;
 }
 
@@ -127,8 +127,7 @@ int fmp_stop()
 	char cmd[100];//容纳cmd的字符串变量。
 	sendCommand("Close All");
 	sleep(1);
-	sprintf(cmd, "taskkill /f /t /im %s.exe",__argv[0]);//写入cmd命令
-	//printf("%s",cmd);
+	sprintf(cmd, "taskkill /f /t /im %s.exe",__argv[0]);
 	WinExec(cmd,SW_HIDE);
 	return 0;
 }
@@ -142,12 +141,8 @@ int fmp_singleplay()
 		sprintf(msg,"Cannot shorten filename \"%s\"\n",__argv[1]);
 		MessageBox(NULL,msg,"Fmediaplay",MB_OK);
 		return 1;
-	}  
-	sendCommand("Close All");
-	sprintf(cmdBuff,"Open %s Type MPEGVideo Alias theMP3",shortBuffer);
-	sendCommand(cmdBuff);
-	sendCommand("Play theMP3 Wait");
-	//printf("单次\n %s %s %s %d",__argv[0],__argv[1],__argv[2],__argc);
+	}
+	fmp_play(shortBuffer);
 	return 0;
 }
 
@@ -157,19 +152,17 @@ int fmp_list()
 {	
 
 	int i,j;
-	char listname[31][100], *ext[1];//定义显示例表的数组长度,和一个存放后缀名的指针数组
-	ext[1]="*.mp3";//可以用下一句指定后缀名
-	//scanf("%s",&*ext[1]);
+	char *ext[1];//定义存放后缀名的指针数组
+	ext[1]="*.mp3";//可以用下一句指定后缀名//scanf("%s",&*ext[1]);
 	
     WIN32_FIND_DATA p;
-    HANDLE h=FindFirstFile(ext[1],&p);//见文尾注释,本例中在当前目录搜索mp3文件
-    //puts(p.cFileName);//去掉注释，帮你理解此时输出了什么
+    HANDLE h=FindFirstFile(ext[1],&p);//当前目录搜索mp3文件
+
 	strcpy(listname[1],p.cFileName);//第一次写入数组
 	printf("%d %s\n",1,listname[1]);
 	
 	i=2;//listname[1]赋值过了，listname[0]就让它空着吧
 	while(FindNextFile(h,&p)){
-		//puts(p.cFileName);//去掉注释，帮你理解此时输出了什么
 		strcpy(listname[i],p.cFileName);
 		printf("%d %s\n",i,listname[i]);
 		i++;
@@ -180,14 +173,8 @@ int fmp_list()
 	}
 	j=i;
 	for(i=1;i<j;i++){
-		sendCommand("Close All");
-		sprintf(cmdBuff,"Open \"%s\" Type MPEGVideo Alias theMP3",listname[i]);
-		sendCommand(cmdBuff);
-		sendCommand("Play theMP3 Wait");
-		//mciSendString("play theMP3 Wait",NULL,0,0);//等同于上条命令
+		fmp_play(listname[i]);
 	}
-	//printf("记录一%s 记录二%s\n",listname[1],listname[3]);//验证数组内容用的
-	//system("pause");
     return 0;
 }
 
@@ -203,13 +190,8 @@ int fmp_singlerepeat()
 		}  
 		
 		for(int i=1;i<99;i++){
-		sendCommand("Close All");
-		sprintf(cmdBuff,"Open %s Type MPEGVideo Alias theMP3",shortBuffer);
-		sendCommand(cmdBuff);
-		sendCommand("Play theMP3 Wait");
-		//mciSendString("play theMP3 Wait",NULL,0,0);//等同于上条命令
+			fmp_play(shortBuffer);
 		}
-	//printf("单次\n %s %s %s %d",__argv[0],__argv[1],__argv[2],__argc);
 	return 0;
 }
 
@@ -219,12 +201,11 @@ int fmp_point()
 {	
 
 	int i,j;
-	char listname[31][100], *ext[1];//定义显示例表的数组长度,和一个存放后缀名的指针数组
-	ext[1]="*.mp3";//可以用下一句指定后缀名
-	//scanf("%s",&*ext[1]);
+	char *ext[1];//存放后缀名的指针数组
+	ext[1]="*.mp3";//可以用下一句指定后缀名//scanf("%s",&*ext[1]);
 	
     WIN32_FIND_DATA p;
-    HANDLE h=FindFirstFile(ext[1],&p);//见文尾注释,本例中在当前目录搜索mp3文件
+    HANDLE h=FindFirstFile(ext[1],&p);//当前目录搜索mp3文件
     //puts(p.cFileName);//去掉注释，帮你理解此时输出了什么
 	strcpy(listname[1],p.cFileName);//第一次写入数组
 	printf("%d %s\n",1,listname[1]);
@@ -251,14 +232,8 @@ int fmp_point()
 	printf("播放的歌曲序号: %d\n", i);
 
 	for(;i<j;i++){
-		sendCommand("Close All");
-		sprintf(cmdBuff,"Open \"%s\" Type MPEGVideo Alias theMP3",listname[i]);
-		sendCommand(cmdBuff);
-		sendCommand("Play theMP3 Wait");
-		//mciSendString("play theMP3 Wait",NULL,0,0);//等同于上条命令
+		fmp_play(listname[i]);
 	}
-	//printf("记录一%s 记录二%s\n",listname[1],listname[3]);//验证数组内容用的
-	//system("pause");
     return 0;
 }
 
@@ -268,19 +243,16 @@ int fmp_pointrepeat()
 {	
 
 	int i,j;
-	char listname[31][100], *ext[1];//定义显示例表的数组长度,和一个存放后缀名的指针数组
-	ext[1]="*.mp3";//可以用下一句指定后缀名
-	//scanf("%s",&*ext[1]);
+	char *ext[1];//存放后缀名的指针数组
+	ext[1]="*.mp3";//可以用下一句指定后缀名scanf("%s",&*ext[1]);
 	
     WIN32_FIND_DATA p;
-    HANDLE h=FindFirstFile(ext[1],&p);//见文尾注释,本例中在当前目录搜索mp3文件
-    //puts(p.cFileName);//去掉注释，帮你理解此时输出了什么
+    HANDLE h=FindFirstFile(ext[1],&p);//当前目录搜索mp3文件
 	strcpy(listname[1],p.cFileName);//第一次写入数组
 	printf("%d %s\n",1,listname[1]);
 	
 	i=2;//listname[1]赋值过了，listname[0]就让它空着吧
 	while(FindNextFile(h,&p)){
-		//puts(p.cFileName);//去掉注释，帮你理解此时输出了什么
 		strcpy(listname[i],p.cFileName);
 		printf("%d %s\n",i,listname[i]);
 		i++;
@@ -294,19 +266,25 @@ int fmp_pointrepeat()
 	j=i;
 	i=atoi(__argv[2]);
 	sprintf(msg,"点击确定循环播放[当前歌曲］:\n\n当前歌曲%d %s",i,listname[i]);
-	//MessageBox(NULL,msg,"fmp",MB_OKCANCEL);
+	//函数原形MessageBox(NULL,msg,"fmp",MB_OKCANCEL);
 	if(MessageBox(NULL,msg,"fmp",MB_OKCANCEL)==IDCANCEL)  
 	return 0; 
 	printf("播放的歌曲序号: %d\n", i);
 
 	for(;i<j;){
-		sendCommand("Close All");
-		sprintf(cmdBuff,"Open \"%s\" Type MPEGVideo Alias theMP3",listname[i]);
-		sendCommand(cmdBuff);
-		sendCommand("Play theMP3 Wait");
-		//mciSendString("play theMP3 Wait",NULL,0,0);//等同于上条命令
+		fmp_play(listname[i]);
 	}
-	//printf("记录一%s 记录二%s\n",listname[1],listname[3]);//验证数组内容用的
-	//system("pause");
     return 0;
+}
+
+int fmp_play(char *filename)
+{
+	//void sendCommand(char *s);
+	
+	sendCommand("Close All");
+	sprintf(cmdBuff,"Open \"%s\" Type MPEGVideo Alias theMP3",filename);
+	sendCommand(cmdBuff);
+	sendCommand("Play theMP3 Wait");
+	//mciSendString("play theMP3 Wait",NULL,0,0);//等同于上条命令
+	return 0;
 }
